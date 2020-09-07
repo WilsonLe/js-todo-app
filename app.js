9f518ffa6cc811e9f71bffb29acc40d2aeaa9b93
WilsonLe <leanhminh2907@gmail.com>
2020-09-07 11:04:01 +0700

    save. this is a fire dumpster of async await

--- app.js
+++ app.js
@@ -2,12 +2,14 @@
 const todoInput = document.querySelector('.todo-input')
 const todoButton = document.querySelector('.todo-button')
 const todoList = document.querySelector('.todo-list')
-const todoRemove = document.querySelector('.todo-remove')
+
 
 //event listeners
 todoButton.addEventListener('click', (e) => {
+    id = Date.now();
     e.preventDefault();
-    addTodo();
+    addTodo(id, addRemoveListener);
+
 })
 todoInput.addEventListener('change', () => {
     addTask();
@@ -15,41 +17,56 @@ todoInput.addEventListener('change', () => {
 
 
 
-
 //init todo lists
 let todos = [];
 
 //functions
-function addTodo() {
+
+function addTodo(id, callback) {
     text = addTask();
-    id = Date.now();
-
-    //processing text
-    todos.push(`
-        <div class="todo-item" data-id="${id}">
-            <div class="todo-text">
-                ${text}
+    if (text != '') {
+        //processing text
+        todos.push([`
+            <div class="todo-item" data-id="${id}">
+                <div class="todo-text">
+                    ${text}
+                </div>
+                <button class="todo-remove" data-id="${id}" id="${id}">
+                    <i class="far fa-trash-alt"></i>
+                </button>
             </div>
-            <button class="todo-remove" data-id="${id}">
-                <i class="far fa-trash-alt"></i>
-            </button>
-        </div>
-    `)
+        `,id])
 
 
-    //render to dom
-    var DOM = ``
-    for (i = 0; i < todos.length; i++) {
-        DOM += todos[i];
+        //render to dom
+        var DOM = ``
+        for (i = 0; i < todos.length; i++) {
+            DOM += todos[i];
+        }
+        todoList.innerHTML = DOM
+
+        //clear inputs
+        clearInputs();
     }
-    todoList.innerHTML = DOM
-
-    //clear inputs
-    clearInputs();
+    callback(id);
 }
 function addTask() {
     return todoInput.value;
 }
 function clearInputs() {
     todoInput.value = '';
+}
+function addRemoveListener(id) {
+    document.getElementById(id).addEventListener('click', () => {
+        for (i = 0 ; i < todos.length; i++){
+            if (todos[i][1] == id){
+                todos.splice(i,1)
+            }
+        }
+        var DOM = ``
+        for (i = 0; i < todos.length; i++) {
+            DOM += todos[i];
+        }
+        todoList.innerHTML = DOM
+    })
 }
