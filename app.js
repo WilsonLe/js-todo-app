99600b2938f7619b07e00c332c0ad8265f1fe223
WilsonLe <leanhminh2907@gmail.com>
2020-09-08 13:37:26 +0700

    add storage

--- app.js
+++ app.js
@@ -1,4 +1,32 @@
-let todos = []
+class Todos{
+    getTodos(){
+        var todos = Storage.getTodos();
+        return todos
+    }
+    renderTodos(input){
+        let DOM = ``
+        for (var i = 0; i < todos.length; i++) {
+            DOM += todos[i][1];
+        }
+        const todoList = document.querySelector('.todo-list')
+        todoList.innerHTML = DOM
+    }
+}
+
+class Storage{
+    static getTodos(){
+        if (localStorage.getItem('Todos')){
+            return JSON.parse(localStorage.getItem('Todos'))
+        }else{
+            return []
+        }
+    }
+    static saveTodos(todos){
+        localStorage.setItem('Todos', JSON.stringify(todos))
+    }
+}
+
+
 class UI {
     setupApp() {
         const todoButton = document.querySelector('.todo-button')
@@ -7,10 +35,10 @@ class UI {
             var text = this.getInput()
             this.clearInput()
             this.renderInput(text).then(() => {
+                Storage.saveTodos(todos)
                 this.getRemoveButtons()
             })
         })
-
     }
     getInput() {
         const todoInput = document.querySelector('.todo-input')
@@ -71,6 +99,7 @@ class UI {
                 todos.splice(i, 1)
             }
         }
+        Storage.saveTodos(todos)    
     }
 }
 
@@ -80,5 +109,9 @@ class UI {
 
 document.addEventListener('DOMContentLoaded', () => {
     ui = new UI
+    todo = new Todos
+    todos = todo.getTodos()
+    todo.renderTodos(todos)
+    ui.getRemoveButtons()
     ui.setupApp()
 })
